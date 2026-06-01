3c031869770e47d8e1ca62a97966c29f4ad636be
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-06-01 15:04:26 +0300

    great

--- swimming/settings/production.py
+++ swimming/settings/production.py
@@ -3,13 +3,14 @@ from swimming.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    'wantedpa.pythonanywhere.com/',
-    'www.wantedpa.pythonanywhere.com/'
+    'wantedpa.pythonanywhere.com',
+    'www.wantedpa.pythonanywhere.com'
 ]
 
 STATIC_ROOT = '/home/wantedpa/swimming/static'
+STATIC_URL = '/static/'
 
-# Безопасность в продакшене
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# # Безопасность в продакшене
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
