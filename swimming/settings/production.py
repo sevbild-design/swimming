fd531f942c1169be8e3f8c0753d8c4690ed70668
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-06-02 00:12:14 +0300

    new static

--- swimming/settings/production.py
+++ swimming/settings/production.py
@@ -4,8 +4,9 @@ from swimming.settings.base import *
 DEBUG = False
 ALLOWED_HOSTS = ['89.108.76.38']
 
-STATIC_ROOT = '/var/www/swimming_static'
-STATIC_URL = '/static/'
+# STATIC_ROOT = '/var/www/swimming_static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # # Безопасность в продакшене
 # SECURE_SSL_REDIRECT = True
