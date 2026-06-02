6d7f604de25e13c8e0ee30be0f88fb78fd128f4e
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-06-03 01:32:07 +0300

    sevbild

--- swimming/settings/development.py
+++ swimming/settings/development.py
@@ -2,7 +2,13 @@ from swimming.settings.base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1',
+    'sevbild.ru',
+    'www.sevbild.ru',
+    'sevbild.online',
+    'www.sevbild.online',    
+]
 
 # Дополнительные настройки для разработки
 INTERNAL_IPS = ['127.0.0.1']  # для django-debug-toolbar
