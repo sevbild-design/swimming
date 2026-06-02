2123a03644d2f235393cfb933709c8a2e960d823
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-06-03 01:38:34 +0300

    sevbild

--- swimming/settings/development.py
+++ swimming/settings/development.py
@@ -4,10 +4,6 @@ from swimming.settings.base import *
 DEBUG = True
 ALLOWED_HOSTS = [
     'localhost', '127.0.0.1',
-    'sevbild.ru',
-    'www.sevbild.ru',
-    'sevbild.online',
-    'www.sevbild.online',    
 ]
 
 # Дополнительные настройки для разработки
--- swimming/settings/production.py
+++ swimming/settings/production.py
@@ -2,6 +2,12 @@ from swimming.settings.base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['130.49.151.130']
+ALLOWED_HOSTS = [
+    '130.49.151.130',
+    'sevbild.ru',
+    'www.sevbild.ru',
+    'sevbild.online',
+    'www.sevbild.online',
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
