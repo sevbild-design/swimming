5ee9e76b8e1824ed9ee7762c8cdb5838a33d576c
PavelMilyutikov <sevastopolrem@yandex.ru>
2026-06-02 22:44:32 +0300

    new ip

--- swimming/settings/production.py
+++ swimming/settings/production.py
@@ -2,8 +2,10 @@ from swimming.settings.base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['194.226.142.39']
+ALLOWED_HOSTS = ['130.49.151.130']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
+ssh-copy-id root@194.226.142.39
+ssh root@<IP-адрес_вашего_сервера>
